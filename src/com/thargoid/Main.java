e33e45ac095ea7a274b15e041685d6ff1876cf36
co17 <clifforeilly@gmail.com>
2017-02-20 16:15:41 +0000

    update aru 2

--- src/com/thargoid/Main.java
+++ src/com/thargoid/Main.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
-
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.Annotation;
@@ -17,6 +16,8 @@ import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.util.CoreMap;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+import de.saar.coli.salsa.reiter.framenet.*;
+import de.saar.coli.salsa.reiter.framenet.FrameNet;
 
 //args
 //0=process type
@@ -57,6 +58,12 @@ public class Main {
     static List<String> pronounNodeNames;
     static StanfordCoreNLP pipeline;
     static int NumParsedColumns = 7;
+    static String inFolder;
+    static FrameNet fn;
+    static String[] row;
+    static String[] prevRow;
+    static String[] rowout;
+    static String lastRow = "1";
 
     public static void main(String[] args) {
 
@@ -123,7 +130,8 @@ public class Main {
                         //2 - csv line per sentence with associated extra detail
                         //3 - csv two first columns are sentences to be parsed
                         setupParseLookups();
-                        Parse(WorkFolder + File.separator + "1_in", "1", "1");
+                        Parse("1", "1");
+                        WorkFolder = WorkFolder.replace(inFolder, "2_parsed");
                     }
                     catch(Exception ex)
                     {
@@ -143,12 +151,17 @@ public class Main {
                     break;
 
                 case 'f':
-
-                    //framenet
                     log("Starting Framenet");
-
+                    try {
+                        //framenet
+                        Framer ("1");
+                        WorkFolder = WorkFolder.replace(inFolder, "3_framed");
+                    }
+                    catch(Exception ex)
+                    {
+                        log(ex.getMessage());
+                    }
                     log("Ending Framenet");
-
                     break;
 
                 case 'g':
@@ -197,19 +210,463 @@ public class Main {
 
     }
 
-    static private boolean Parse(String pParseFolder, String pInputType, String pOutputType)
+
+    static private boolean Framer(String pOutputType)
     {
         boolean output = false;
-        String ParseFolder = pParseFolder;
+        String FramerFolder = WorkFolder + File.separator + inFolder;
+        String OutputType = pOutputType;
+
+        String outputFolder = FramerFolder.replace(inFolder, "3_framed");
+        inFolder = "3_framed";
+        Path p = Paths.get(FramerFolder);
+
+        try {
+            if (Files.notExists(p)) {
+                Files.createDirectory(p);
+                log("Created directory " + p);
+            }
+
+            if (deleteFiles) {
+                File f = new File(outputFolder);
+                File[] matchingFiles = f.listFiles();
+
+                if (matchingFiles != null) {
+                    int c = 0;
+                    for (File tf : matchingFiles) {
+                        tf.delete();
+                        c++;
+                    }
+                    log("Deleted " + c + " files");
+                }
+            }
+
+            File f = new File(WorkFolder);
+            File[] matchingFiles = f.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.toLowerCase().endsWith(".csv");
+                }
+            });
+            log("Scanned " + WorkFolder + " for csv files");
+
+            int filecount = 0;
+            String FrameNetFolder = "C:\\Users\\co17\\LocalStuff\\MyStuff\\Personal\\MPhil\\Framenet\\fndata-1.7\\fndata-1.7";
+            int luCol = 7;
+            int posCol = 4;
+
+            fn = new FrameNet();
+            File fnHome = new File(FrameNetFolder);
+            DatabaseReader reader = new FNDatabaseReader15(fnHome,true);
+            fn.readData(reader);
+            log("Set up framenet objects with folder " + FrameNetFolder);
+
+            List<String[]> LUs = new ArrayList<String[]>();
+            for(Frame fr : fn.getFrames())
+            {
+                for(LexicalUnit luv : fr.getLexicalUnits())
+                {
+                    String[] tmpLU = new String[3];
+                    tmpLU[0]=luv.getLexemeString();
+                    tmpLU[1]=luv.getPartOfSpeechAbbreviation();
+                    tmpLU[2]=fr.getName();
+                    LUs.add(tmpLU);
+                }
+            }
+            log("Set up local LU List");
+
+            String[] row;
+
+            int rowcount = 0;
+            for(File tf : matchingFiles)
+            {
+                filecount++;
+                CSVReader csvinput = new CSVReader(new FileReader(tf.getAbsolutePath()));
+                log("Reading csv file ... " + tf.getAbsolutePath());
+                List csvinputdata = csvinput.readAll();
+                csvinput.close();
+
+                String newFilename = outputFolder + File.separator + "ms-" + tf.getName();
+                CSVWriter csvout = new CSVWriter(new FileWriter(newFilename));
+                log("New file opened ... " + newFilename);
+
+                List<String> framesSoFar = new ArrayList<String>();
+                List<String> SuppliedFramesO  = new ArrayList<String>();
+
+                for(Object ob : csvinputdata)
+                {
+                    List<String> frames = new ArrayList<String>();
+                    row=(String[]) ob;
+                    String tLU = row[luCol];
+                    String tPOS = row[posCol];
+
+                    boolean init = false;
+
+                    for(String[] strlus : LUs)
+                    {
+                        if(strlus[0].trim().equals(tLU) && strlus[1].equals(tPOS))
+                        {
+                            log("Checking " + strlus[0] + ", " + strlus[1] + ", " + strlus[2]);
+                            frames.add(strlus[2]);
+                            log("Added frame " + strlus[2]);
+                            framesSoFar.add(strlus[2]);
+                            init=true;
+                        }
+                    }
+
+                    int colCount = 30; //row.length + parsedColumns
+                    int parsedColumns = 22;
+
+                    if(init)
+                    {
+                        csvout.writeNext(writeFrameData(OutputType, colCount, frames, tLU, "False"));
+                        rowcount++;
+                        log("Written a row: " + rowcount);
+                    }
+                    else // no frames
+                    {
+                        log("No frames!");
+                        rowout = new String[colCount];
+                        int a = 0;
+
+                        for(String s : row)
+                        {
+                            rowout[a] = s;
+                            a++;
+                        }
+
+                        for(int n = 0 ; n<parsedColumns ; n++)
+                        {
+                            rowout[a+n] = "";
+                        }
+
+                        csvout.writeNext(rowout);
+                        rowcount++;
+                    }
+
+                    lastRow = row[0];
+                    prevRow = row;
+
+                }
+
+                csvout.close();
+            }
+
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+        return output;
+    }
+
+
+    static public List<String> getFrameElements(String frame)
+    {
+        List<String> outputFrameElements = new ArrayList<String>();
+        try
+        {
+            Frame f = fn.getFrame(frame);
+            for(FrameElement fe : f.getFrameElements().values())
+            {
+                outputFrameElements.add(fe.getName());
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Error:-" + ex.toString() + ", " + ex.getMessage() + ", " + ex.getLocalizedMessage());
+            ex.printStackTrace();
+
+        }
+        return outputFrameElements;
+    }
+
+    static public List<String> getFrameLUs(String frame)
+    {
+        List<String> outputLUs = new ArrayList<String>();
+        try
+        {
+            Frame f = fn.getFrame(frame);
+            for(LexicalUnit lu : f.getLexicalUnits())
+            {
+                outputLUs.add(lu.getName());
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Error:-" + ex.toString() + ", " + ex.getMessage() + ", " + ex.getLocalizedMessage());
+            ex.printStackTrace();
+
+        }
+        return outputLUs;
+    }
+
+
+    static public String[] writeFrameData(String type, int colCount, List<String> frames, String tLU, String extraLine)
+    {
+        //colCount = row.length + parsedColumns
+        try
+        {
+            if(type=="1")  //basic framenet spacers
+            {
+                rowout = new String[colCount];
+
+                for(int h=0 ; h<colCount ; h++)
+                {
+                    rowout[h]="";
+                }
+
+                for(String frnm : frames)
+                {
+                    Frame fr = fn.getFrame(frnm);
+                    int a = 0;
+
+                    if(extraLine.equals("True"))
+                    {
+                        for(String s : prevRow)
+                        {
+                            rowout[a] = s;
+                            a++;
+                        }
+                        rowout[prevRow.length-1]="";
+                        rowout[prevRow.length-2]="";
+                        rowout[prevRow.length-3]="";
+                        rowout[prevRow.length-4]="";
+                        rowout[prevRow.length-5]="";
+                        rowout[prevRow.length-6]="0";
+                    }
+                    else
+                    {
+                        for(String s : row)
+                        {
+                            rowout[a] = s;
+                            a++;
+                        }
+                    }
+
+                    //add Frame name
+                    rowout[a]=rowout[a] + " " + fr.getName();
+                    a++;
+
+                    //add Frame Elements
+                    String FEs = "";
+                    for(String FE : getFrameElements(fr.getName()))
+                    {
+                        FEs = FEs + FE + " ";
+                    }
+                    FEs.trim();
+                    rowout[a]=rowout[a] + " " + FEs;
+                    a++;
+
+                    //add Frame LUs
+                    String fLUs = "";
+                    for(String fLU : getFrameLUs(fr.getName()))
+                    {
+                        fLUs = fLUs + fLU + " ";
+                    }
+                    fLUs.trim();
+                    rowout[a]=rowout[a] + " " + fLUs;
+                    a++;
+
+                    String ibFs = "";
+                    for(Frame IdF : fr.isInheritedBy())
+                    {
+                        ibFs = ibFs + IdF + " ";
+                    }
+                    ibFs.trim();
+                    rowout[a]=rowout[a] + " " + ibFs;
+                    a++;
+
+                    String pFs = "";
+                    for(Frame IdF : fr.perspectivized())
+                    {
+                        pFs = pFs + IdF + " ";
+                    }
+                    pFs.trim();
+                    rowout[a]=rowout[a] + " " + pFs;
+                    a++;
+
+                    String uFs = "";
+                    for(Frame IdF : fr.uses())
+                    {
+                        uFs = uFs + IdF + " ";
+                    }
+                    uFs.trim();
+                    rowout[a]=rowout[a] + " " + uFs;
+                    a++;
+
+                    String ubFs = "";
+                    for(Frame IdF : fr.usedBy())
+                    {
+                        ubFs = ubFs + IdF + " ";
+                    }
+                    ubFs.trim();
+                    rowout[a]=rowout[a] + " " + ubFs;
+                    a++;
+
+                    String hsfFs = "";
+                    for(Frame IdF : fr.hasSubframe())
+                    {
+                        hsfFs = hsfFs + IdF + " ";
+                    }
+                    hsfFs.trim();
+                    rowout[a]=rowout[a] + " " + hsfFs;
+                    a++;
+
+                    String incFs = "";
+                    for(Frame IdF : fr.inchoative())
+                    {
+                        incFs = incFs + IdF + " ";
+                    }
+                    incFs.trim();
+                    rowout[a]=rowout[a] + " " + incFs;
+                    a++;
+
+                    String incsFs = "";
+                    for(Frame IdF : fr.inchoativeStative())
+                    {
+                        incsFs = incsFs + IdF + " ";
+                    }
+                    incsFs.trim();
+                    rowout[a]=rowout[a] + " " + incsFs;
+                    a++;
+
+                    String cauFs = "";
+                    for(Frame IdF : fr.causative())
+                    {
+                        cauFs = cauFs + IdF + " ";
+                    }
+                    cauFs.trim();
+                    rowout[a]=rowout[a] + " " + cauFs;
+                    a++;
+
+                    String caustFs = "";
+                    for(Frame IdF : fr.causativeStative())
+                    {
+                        caustFs = caustFs + IdF + " ";
+                    }
+                    caustFs.trim();
+                    rowout[a]=rowout[a] + " " + caustFs;
+                    a++;
+
+                    String aifFs = "";
+                    for(Frame IdF : fr.allInheritedFrames())
+                    {
+                        aifFs = aifFs + IdF + " ";
+                    }
+                    aifFs.trim();
+                    rowout[a]=rowout[a] + " " + aifFs;
+                    a++;
+
+                    String aigfFs = "";
+                    for(Frame IdF : fr.allInheritingFrames())
+                    {
+                        aigfFs = aigfFs + IdF + " ";
+                    }
+                    aigfFs.trim();
+                    rowout[a]=rowout[a] + " " + aigfFs;
+                    a++;
+
+                    String earFs = "";
+                    for(Frame IdF : fr.earlier())
+                    {
+                        earFs = earFs + IdF + " ";
+                    }
+                    earFs.trim();
+                    rowout[a]=rowout[a] + " " + earFs;
+                    a++;
+
+                    String ifFs = "";
+                    for(Frame IdF : fr.inheritsFrom())
+                    {
+                        ifFs = ifFs + IdF + " ";
+                    }
+                    ifFs.trim();
+                    rowout[a]=rowout[a] + " " + ifFs;
+                    a++;
+
+                    String lFs = "";
+                    for(Frame IdF : fr.later())
+                    {
+                        lFs = lFs + IdF + " ";
+                    }
+                    lFs.trim();
+                    rowout[a]=rowout[a] + " " + lFs;
+                    a++;
+
+                    String nFs = "";
+                    for(Frame IdF : fr.neutral())
+                    {
+                        nFs = nFs + IdF + " ";
+                    }
+                    nFs.trim();
+                    rowout[a]=rowout[a] + " " + nFs;
+                    a++;
+
+                    String refFs = "";
+                    for(Frame IdF : fr.referred())
+                    {
+                        refFs = refFs + IdF + " ";
+                    }
+                    refFs.trim();
+                    rowout[a]=rowout[a] + " " + refFs;
+                    a++;
+
+                    String refrFs = "";
+                    for(Frame IdF : fr.referring())
+                    {
+                        refrFs = refrFs + IdF + " ";
+                    }
+                    refrFs.trim();
+                    rowout[a]=rowout[a] + " " + refrFs;
+                    a++;
+
+                    String sfoFs = "";
+                    for(Frame IdF : fr.subframeOf())
+                    {
+                        sfoFs = sfoFs + IdF + " ";
+                    }
+                    sfoFs.trim();
+                    rowout[a]=rowout[a] + " " + sfoFs;
+                    a++;
+
+                    if(extraLine.equals("True"))
+                    {
+                        rowout[a]="";
+                    }
+                    else
+                    {
+                        rowout[a]=tLU;
+                    }
+                }
+            }
+            if(type=="2")  //some more advanced spacers
+            {
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Error:-" + ex.toString() + ", " + ex.getMessage() + ", " + ex.getLocalizedMessage());
+            ex.printStackTrace();
+        }
+
+        return rowout;
+    }
+
+
+    static private boolean Parse(String pInputType, String pOutputType)
+    {
+        boolean output = false;
+        String ParseFolder = WorkFolder + File.separator + inFolder;
         String InputType = pInputType;
         //1 - plain text file
         //2 - csv line per sentence with associated extra detail
         //3 - csv two first columns are sentences to be parsed
         String OutputType = pOutputType;
 
-
-        String inFolder = "1_in";
         String outputFolder = ParseFolder.replace(inFolder, "2_parsed");
+        inFolder = "2_parsed";
         Path p = Paths.get(outputFolder);
 
         try {
@@ -371,6 +828,7 @@ public class Main {
         try {
             quando = getNow();
             LogFileName = WorkFolder + File.separator + "log-" + quando + ".txt";
+            inFolder = "1_in";
             log("Finished setup");
         }
         catch (Exception ex)
